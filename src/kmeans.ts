f25f375d07561dc82c2dce75ec96b3b64f161f94
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-07-02 00:33:46 +0800

    Update kmeans.ts

--- src/kmeans.ts
+++ src/kmeans.ts
@@ -1,13 +1,12 @@
 import { toPixel, RGBA, euclidean_distance } from "./util"
 
-export default function kmeans(img: ImageData, k: number, attempt: number):KMeansResult {
+export default function kmeans(data: RGBA[], k: number, attempt: number):KMeansResult {
     const THRESOLD = 1
-    const data = toPixel(img)
-    const cluster_center: RGBA[] = new Array(k)
-    const new_cluster_center = new Array(k)
+    const cluster_centers: RGBA[] = new Array(k)
+    const new_cluster_centers = new Array(k)
     //随机选点
     for (let i = 0; i < k; i++) {
-        cluster_center[i] = data[Math.floor(Math.random() * data.length)]
+        cluster_centers[i] = data[Math.floor(Math.random() * data.length)]
     }
     let iterate_time = 0
     const cluster_sum = new Array(k).fill(0).map(() => new Array(5).fill(0))//[r,g,b,a,c]
@@ -17,9 +16,9 @@ export default function kmeans(img: ImageData, k: number, attempt: number):KMean
         for (let i = 0; i < data.length; i++) {
             const data_item = data[i]
             let cluster_index = 0
-            let _min_distance = euclidean_distance(data_item, cluster_center[0])
+            let _min_distance = euclidean_distance(data_item, cluster_centers[0])
             for (let j = 1; j < k; j++) {
-                const distance = euclidean_distance(data_item, cluster_center[j])
+                const distance = euclidean_distance(data_item, cluster_centers[j])
                 if (distance < _min_distance) {
                     _min_distance = distance
                     cluster_index = j
@@ -36,23 +35,25 @@ export default function kmeans(img: ImageData, k: number, attempt: number):KMean
         for (let i = 0; i < k; i++) {
             const rgbac = cluster_sum[i]
             const count = rgbac[4]
-            if (count == 0) new_cluster_center[i] = data[Math.floor(Math.random() * data.length)]
-            else new_cluster_center[i] = [rgbac[0] / count, rgbac[1] / count, rgbac[2] / count, rgbac[3] / count]
+            if (count == 0) new_cluster_centers[i] = data[Math.floor(Math.random() * data.length)]
+            else {
+                new_cluster_centers[i] = [rgbac[0] / count, rgbac[1] / count, rgbac[2] / count, rgbac[3] / count]
+            }
         }
         let diff = 0
         for (let i = 0; i < k; i++) {
-            diff += euclidean_distance(cluster_center[i], new_cluster_center[i])
+            diff += euclidean_distance(cluster_centers[i], new_cluster_centers[i])
         }
         if (diff <= THRESOLD) {
             return {
-                cluster_center: new_cluster_center, iterate_time, fit_thresold: true, label: cluster_sum.map(v => v[4])
+                cluster_center: new_cluster_centers, iterate_time, fit_thresold: true, label: cluster_sum.map(v => v[4])
             }
         }
-        _swap_array(new_cluster_center, cluster_center)
+        _swap_array(new_cluster_centers, cluster_centers)
         iterate_time++
     }
     return {
-        cluster_center, iterate_time, fit_thresold: false, label: cluster_sum.map(v => v[4])
+        cluster_center: cluster_centers, iterate_time, fit_thresold: false, label: cluster_sum.map(v => v[4])
     }
 }
 export interface KMeansResult {

4c85d37e7c411c1a158c042313dd422a4ef9e3a0
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-11-10 18:43:44 +0800

    update example

--- example/app.ts
+++ example/app.ts
@@ -1,6 +1,6 @@
 
-import { rgbaToHSLA, normalizeRGBA, sortHSL, RGBA, readImageDownsampling, toPixel, readImage } from "../src/util";
 import { kmeanWorkerData } from "./worker";
+import { readImageDownsampling, toPixel, readImage, rgbaToHSLA, normalizeRGBA, RGBA, sortHSL, toPixelLAB, labaToRGBA } from '../src/index'
 const img = document.getElementsByTagName('img')[0];
 const div_result = document.getElementById('result');
 
@@ -12,21 +12,27 @@ const div_result = document.getElementById('result');
     }
 })
 const workers = new Array(5).fill(0).map(() => new Worker('./worker.ts'))
-function run() {
-
-    cleanResult()
-    const DEV_DOWNSAMPLING = false
-    const data = toPixel(DEV_DOWNSAMPLING ? readImageDownsampling(img, 100 * 1000)! : readImage(img))
-    workers.forEach((worker) => {
+let RESULT = []
+function run(laba = false) {
+    const DEV_DOWNSAMPLING = true
+    const data = (laba ? toPixelLAB : toPixel)(DEV_DOWNSAMPLING ? readImageDownsampling(img, 100 * 1000)! : readImage(img))
+    return Promise.all(workers.map((worker) => new Promise<void>(resolve => {
         worker.postMessage({ img: data, k: parseInt((document.getElementById('k') as HTMLInputElement).value), attempt: 100 } as kmeanWorkerData)
         worker.onmessage = (e) => {
             const { time, result } = e.data
             const row = document.createElement('div')
             const span_time = document.createElement('span')
-            span_time.innerText = time + 'ms' + ',iterate:' + result.iterate_time
+            span_time.innerText = `${time}ms,iterate:${result.iteration}, laba:${laba}`
+            RESULT.push({
+                time, iteration: result.iteration, laba
+            })
             console.log(e.data);
-            (result.cluster_center as RGBA[])
-                .map(v => rgbaToHSLA(normalizeRGBA(v)))
+            (result.centroid as RGBA[])
+                .map(centre => rgbaToHSLA(
+                    normalizeRGBA(
+                        laba ? labaToRGBA(centre as any) : centre
+                    )
+                ))
                 .sort(sortHSL([2, 0, 1, 3]))
                 .forEach(([h, s, l, a]) => {
                     const css_color = `hsla(${h}deg,${s * 100}%,${l * 100}%,${a})`
@@ -38,13 +44,24 @@ function run() {
             row.append(span_time)
 
             div_result?.append(row)
+            resolve()
         }
-    })
-
-
-
+    })))
 }
 function cleanResult() {
     document.querySelectorAll('#result *').forEach(node => node.remove())
+    RESULT = []
 }
-(document.getElementById('run') as HTMLButtonElement).onclick = run
+document.getElementById('run').onclick = () => {
+    run(true).then(() => {
+        run(false)
+    })
+}
+document.getElementById('clear').onclick = () => {
+    cleanResult()
+}
+document.getElementById('show').onclick = () => {
+    console.log(
+        'time,iteration,laba\n'+
+        RESULT.map(({time,iteration,laba})=>`${time},${iteration},${laba}`).join('\n'))
+}

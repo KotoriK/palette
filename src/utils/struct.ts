06f5ee49b61a7f86b1ee57a238aaf71341c6ac56
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-11-10 20:00:34 +0800

    Update struct.ts

--- src/utils/struct.ts
+++ src/utils/struct.ts
@@ -1,3 +1,3 @@
 export type HSLA = [number, number, number, number]
-export type RGBA = [number, number, number, number]
+export type RGBA = [number, number, number, number] | Uint8ClampedArray
 export type LABA = [number, number, number, number]

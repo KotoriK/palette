012afb8d341185c1ad2b6206173ed45c14889a54
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-11-14 23:12:33 +0800

    perf: 减少新数组创建

--- src/kmeans.ts
+++ src/kmeans.ts
@@ -2,34 +2,34 @@ import { euclidean_distance } from "./utils/math"
 import { Vector4, Vector5 } from "./utils/struct"
 
 export default function kmeans(data: Vector4[], k: number, attempt: number, thresold = 1): KMeansResult {
-    const cluster_centers: Vector4[] = []
-    const new_cluster_centers: Vector4[] = []
     const cluster_sum: Vector5[]/*[r,g,b,a,c]*/ = []
+    let cluster_centers: Vector4[] = []
+    let new_cluster_centers: Vector4[] = []
     let iteration = 0
-    //随机选点
     for (let i = 0; i < k; i++) {
-        cluster_centers.push(data[Math.floor(Math.random() * data.length)])
+        cluster_centers.push(Array.from(data[Math.floor(Math.random() * data.length)]) as any)  //随机选点
+        new_cluster_centers.push(_filled_array(0, 4) as Vector4)
         cluster_sum.push(_filled_array(0, 5) as Vector5)
     }
     while (iteration < attempt) {
         //准备坐标和
         //计算每个点与中心的距离
         for (let i = 0; i < data.length; i++) {
-            const data_item = data[i]
+            const rgba = data[i]
             let cluster_index = 0
-            let _min_distance = euclidean_distance(data_item, cluster_centers[0])
+            let _min_distance = euclidean_distance(rgba, cluster_centers[0])
             for (let j = 1; j < k; j++) {
-                const distance = euclidean_distance(data_item, cluster_centers[j])
+                const distance = euclidean_distance(rgba, cluster_centers[j])
                 if (distance < _min_distance) {
                     _min_distance = distance
                     cluster_index = j
                 }
             }
             const sum = cluster_sum[cluster_index]
-            sum[0] += data_item[0]
-            sum[1] += data_item[1]
-            sum[2] += data_item[2]
-            sum[3] += data_item[3]
+            sum[0] += rgba[0]
+            sum[1] += rgba[1]
+            sum[2] += rgba[2]
+            sum[3] += rgba[3]
             sum[4]++
         }
         let diff = 0
@@ -39,18 +39,24 @@ export default function kmeans(data: Vector4[], k: number, attempt: number, thre
             const count = rgbac[4]
             if (count == 0) {
                 //空类 重新选中心点
-                new_cluster_centers[i] = data[Math.floor(Math.random() * data.length)]
+                new_cluster_centers[i] = Array.from(data[Math.floor(Math.random() * data.length)]) as any
+                diff += thresold
             } else {
-                new_cluster_centers[i] = [rgbac[0] / count, rgbac[1] / count, rgbac[2] / count, rgbac[3] / count]
+                const new_center = new_cluster_centers[i]
+                for (let j = 0; j < 4; j++) {
+                    new_center[j] = rgbac[j] / count
+                }
+                diff += euclidean_distance(cluster_centers[i], new_cluster_centers[i])
             }
-            diff += euclidean_distance(cluster_centers[i], new_cluster_centers[i])
         }
         if (diff <= thresold) {
             return {
                 centroid: new_cluster_centers, iteration, fit: true, label: cluster_sum.map(v => v[4]), size: data.length
             }
         }
-        _swap_array(new_cluster_centers, cluster_centers)
+        const medium = cluster_centers
+        cluster_centers = new_cluster_centers
+        new_cluster_centers = medium
         iteration++
         //清空累加
         for (let i = 0; i < k; i++) {
@@ -74,11 +80,6 @@ export interface KMeansResult {
     label: number[]
     /**输入的图像的像素计数 */
     size: number
-}
-function _swap_array(from: Array<any>, to: Array<any>) {
-    for (let i = 0; i < from.length; i++) {
-        to[i] = from[i]
-    }
 }
 function _filled_array<T>(fillWith: T, count: number) {
     const array: T[] = []

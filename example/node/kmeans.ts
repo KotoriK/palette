9078511b3d79c41e8cfd7dd10b1b8d2adae94e53
KotoriK <52659125+KotoriK@users.noreply.github.com>
2022-01-19 16:36:37 +0800

    update

--- example/node/kmeans.ts
+++ example/node/kmeans.ts
@@ -9,18 +9,12 @@ readFile(resolve('./example/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg'))
         const canvas = createCanvas(img.width, img.height)
         const ctx = canvas.getContext('2d')
         ctx.drawImage(img, 0, 0)
-        const {data} = ctx.getImageData(0, 0, img.width, img.height)
-        const obs = new PerformanceObserver((items) => {
-            console.log(items.getEntries()[0].duration);
-            performance.clearMarks();
-        });
-        obs.observe({ entryTypes: ['measure'] })
-        for (let i = 0; i < 5; i++) {
-            performance.mark('runstart')
+        const { data } = ctx.getImageData(0, 0, img.width, img.height)
+        let runstart, runend
+        for (let i = 1; i < 6; i++) {
+            runstart = performance.now()
             const result = kmeans(data, 3, 100)
-            performance.mark('runend')
-            performance.measure('run time', 'runstart', 'runend');
-            console.log(i, ":", { iteration: result.iteration, fit: result.fit })
-            performance.clearMarks();
+            runend = performance.now()
+            console.log(i, ":", { iteration: result.iteration, fit: result.fit }, runend - runstart)
         }
     })
